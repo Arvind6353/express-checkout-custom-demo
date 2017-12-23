8152093d8d8675227c8c180bf46e440fd729f1af
T A <arvta@paypal.com>
2017-12-23 11:19:47 +0530

    Markflow jsv3 changes

--- routes/jsv3-checkout.js
+++ routes/jsv3-checkout.js
@@ -228,16 +228,6 @@ router.get("/success", function(req, res) {
   });
 });
 
-// cancel 
-router.get("/cancel", function(req, res, next) {
-    res.render("cancel", {type:'jsv3'});
-});
-
-// error
-router.get("/error", function(req, res, next) {
-  res.render("error",{message:"", type:'jsv3'});
-});
-
 // create payment and redirect to paypal auth page
 router.post("/confirmation-payment", function(req, res, next) {
   
@@ -372,6 +362,110 @@ router.post("/confirmation-execute", function(req, res) {
 });
 
 
+// create payment with shipping address and redirect to paypal auth page
+router.post("/markflow-payment", function(req, res, next) {
+  
+  console.log('request body ' ,req.body);
+ 
+  var create_payment_json = {
+      intent: "sale",
+      payer: {
+        payment_method: "paypal"
+      },
+      redirect_urls: {
+        return_url: serverUrl+"/api/jsv3/success",
+        cancel_url: serverUrl+"/api/jsv3/cancel"
+      },
+      transactions: [
+        {
+          amount:
+          {
+            total: "4.00",
+            currency: "USD",
+            details:
+            {
+              subtotal: "2.00",
+              shipping: "1.00",
+              tax: "2.00",
+              shipping_discount: "-1.00"
+            }
+          },
+          item_list:
+          {
+            items: [
+            {
+              quantity: "1",
+              name: "item 1",
+              price: "1",
+              currency: "USD",
+              description: "item 1 description",
+              tax: "1"
+            },
+            {
+              quantity: "1",
+              name: "item 2",
+              price: "1",
+              currency: "USD",
+              description: "item 2 description",
+              tax: "1"
+            }],
+            shipping_address: {
+              recipient_name: req.body.recipient_name,
+              line1: req.body.line1,
+              line2: req.body.line2,
+              city: req.body.city,
+              country_code: req.body.country_code,
+              postal_code: req.body.postal_code,
+              state: req.body.state
+          }
+          },
+          description: "The payment transaction description.",
+          custom: "merchant custom data"
+        }]
+      
+    };
+  
+    var payload = create_payment_json; 
+  
+    paypal.configure({
+      mode: req.body.mode || "sandbox", //sandbox or live
+      client_id: req.body.clientId || "AV2UJ4rXMH6vaJcJTUTJR4doweN1og37fTV6xTKIhEPqqmEU7ZuI_Kl86PeTm1EXf6CjdNEixjXmYM7v",
+      client_secret: req.body.clientSecret || "EM1PKF6OWi3lonGwnuCeK8LAfqFr6Rpqbbo-98Ed9hMzNNWOJAvtEMb46m9jVvHjNHKc7kcribk31NrM"
+     });
+  
+     req.session.paypal = paypal;
+     
+    paypal.payment.create(payload, function(
+      err,
+      result
+    ) {
+      if (err) {
+        res.render("error",{message : err, type:'jsv3'});
+      } else {
+        console.log("Create Payment Response");
+        //you forgot to redirect your response to paypal sandbox
+        var redirectUrl;
+        for (var i = 0; i < result.links.length; i++) {
+          var link = result.links[i];
+          if (link.method === "REDIRECT") {
+            redirectUrl = link.href;
+          }
+        }
+        res.redirect(redirectUrl);
+      }
+    });
+  });
+  
+
+// cancel 
+router.get("/cancel", function(req, res, next) {
+  res.render("cancel", {type:'jsv3'});
+});
+
+// error
+router.get("/error", function(req, res, next) {
+  res.render("error",{message:"", type:'jsv3'});
+});
 
 
 module.exports = router;
