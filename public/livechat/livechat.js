5fcda1bf9c8e0d1f02da747be71f18ab90b0a129
T A <arvta@paypal.com>
2017-12-28 10:55:39 +0530

    changes

--- public/livechat/livechat.js
+++ public/livechat/livechat.js
@@ -1,4 +1,6 @@
-    var chat = {
+var guid = generateUUID();
+sessionStorage.setItem('guid',guid);
+var chat = {
       messageToSend: '',
       init: function() {
         this.cacheDOM();
@@ -119,7 +121,7 @@ var html = `
     type: 'POST',
     url: 'https://livechat-help.herokuapp.com/webhook',
     data :{
-      sender :'s1',
+      sender :guid,
       data : datum,
       type : type,
       object: 'web'
@@ -193,4 +195,14 @@ var html = `
   });
 }
 
-  
+function generateUUID () { // Public Domain/MIT
+  var d = new Date().getTime();
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+      d += performance.now(); //use high-precision timer if available
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+}
